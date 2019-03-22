4b51a9385244b50c24cc02ca23a1a856fab0ace5
Abhishek Bhattacharya <babhishek.bhattacharya@gmail.com>
2019-03-22 21:34:25 +0530

    First Unity Game

---
+++ First/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject Sphere;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - Sphere.transform.position;
+    }
+    
+    void LateUpdate ()
+    {
+        transform.position = Sphere.transform.position + offset;
+    }
+}
